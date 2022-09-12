60f262764a300864b401910bf68a18501996983d
Ali Jalilli <1557449@activedir.vu.lt>
2022-09-12 11:21:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewButton;
+    private TextView textViewAbove;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.textViewButton = findViewById(R.id.textViewButton);
+        this.textViewAbove = findViewById(R.id.textViewAbove);
+    }
+
+    public void Click(View view) {
+        textViewButton.setVisibility((textViewButton.getVisibility() == View.VISIBLE)
+                ? View.GONE : View.VISIBLE);
+
+        textViewAbove.setVisibility((textViewAbove.getVisibility() == View.VISIBLE)
+                ? View.GONE : View.VISIBLE);
+
+    }
+}
